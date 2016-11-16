248df6b4b3ec32579d763bbba796159b1ffbfd07
eedick <eedick94@gmail.com>
2016-11-16 12:45:33 -0500

    created different methods for restaurant controller and partials

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,24 +3,45 @@ class RestaurantsController < ApplicationController
 		@restaurants = Restaurant.all  
 	end
 
+	def show
+		@restaurant = Restaurant.find(params[:id])
+	end
+
 	def new
 		@restaurant = Restaurant.new 
 	end 
 
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
+
 	def create
 		@restaurant = current_user.restaurants.build(restaurant_params)
 
-		@restaurant.save
 		if @restaurant.save
-			redirect_to @restaurant
+			redirect_to @restaurant, notice: "Restaurant was created successfully!"
 		else
 			render "new"
 		end	
 	end
 
-	def show
-		@restaurant = Restaurant.find(params[:id])
+	def update
+		@restaurant = Restaurant.find params[:id]
+		if @restaurant.update(restaurant_params)
+			redirect_to @restaurant, notice: "Restaurant was updated successfully!"
+		else 
+			render "edit"
+		end
 	end
+
+	def destroy
+		@restaurant = Restaurant.find params[:id]
+	  @restaurant.destroy
+	  respond_to do |format|
+	   format.html { redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.' }
+	  end
+	end
+
 
 	private
 		def restaurant_params
